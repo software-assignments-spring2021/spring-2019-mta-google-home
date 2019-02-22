12d4fee8189a8593bf825ad3ff757d0ad263b454
Luis Olivar <lao294@nyu.edu>
2019-02-22 12:09:23 -0500

    Setup Babel for ES6 capabilities

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from "express";
+import { urlencoded, json } from "body-parser";
 let app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.listen(port, function() {
   console.log(`Server started on port ${port}`);
