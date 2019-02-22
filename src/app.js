f689840ae277dc5f1ee28d9c9e31532dc2845114
Luis Olivar <lao294@nyu.edu>
2019-02-22 16:51:58 -0500

    Removed body-parser as a dependancy
    Body-parser is now included with express package.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
+import { urlencoded, json } from "body-parser";
 let app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send(`Hello world, from port ${PORT}`);
