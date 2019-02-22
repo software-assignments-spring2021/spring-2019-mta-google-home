3fb13ee732419c32fafbe6f3656abf786647cab3
Luis Olivar <lao294@nyu.edu>
2019-02-22 16:46:41 -0500

    Finalized app setup
    Installed nodemon, fixed start script.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { urlencoded, json } from "body-parser";
+import bodyParser from "body-parser";
 let app = express();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.use(urlencoded({ extended: true }));
-app.use(json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(port, function() {
-  console.log(`Server started on port ${port}`);
+app.get("/", (req, res) => {
+  res.send(`Hello world, from port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
