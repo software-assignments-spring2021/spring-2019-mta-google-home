939d8b0636bd177d2d213817a39103a614d1bc24
DanielY-Yoo <yy2350@nyu.edu>
2019-03-04 18:08:58 -0500

    adding UserInput class

---
+++ src/userInput.js
@@ -0,0 +1,11 @@
+class UserInput {
+    constructor(station, trainType, direction) {
+        this.station = station;
+        this.trainType = trainType;
+        this.direction = direction;
+    }
+}
+
+module.exports = {
+    UserInput
+};
