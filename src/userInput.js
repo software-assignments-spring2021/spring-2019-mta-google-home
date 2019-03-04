f212d45aa71eb45952a759335255324644d96cb7
DanielY-Yoo <yy2350@nyu.edu>
2019-03-04 18:11:22 -0500

    adding currentTime and numTrains to UserInput class

--- src/userInput.js
+++ src/userInput.js
@@ -1,8 +1,12 @@
 class UserInput {
-    constructor(station, trainType, direction) {
+    constructor(station, trainType, direction, numTrains) {
         this.station = station;
         this.trainType = trainType;
         this.direction = direction;
+        this.numTrains = numTrains;
+
+        const currentTime = 1000; // Should set current Time 
+        this.time = currentTime;
     }
 }
 
