1005385608d36fc4d829aee371ef218b095a9efb
Vivian Fang <vf608@nyu.edu>
2019-03-04 23:20:14 -0500

    Adding request to MTA datafeed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { urlencoded, json } from "body-parser";
+import http from "http";
+
 let app = express();
 
 const PORT = process.env.PORT || 5000;
+const KEY = "c5881ea19e9d80654a41b10353585ef0";
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
@@ -13,4 +16,14 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+  const req = http.request(`http://datamine.mta.info/mta_esi.php?key=${KEY}&feed_id=1`, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+  req.end();
 });
