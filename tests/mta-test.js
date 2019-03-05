8657b699d650cb457207601cf0c86f635db4d5ad
Your Name <you@example.com>
2019-03-04 19:44:58 -0500

    adding check userinput time

--- tests/mta-test.js
+++ tests/mta-test.js
@@ -6,4 +6,11 @@ describe('UserInput tests', function() {
     var testInput = new UserInput("Astor Place", "6", "Uptown", 1);
     equal(testInput.station, "Astor Place");
   });
+
+  it('UserInput has Arrival Time ', function(){
+    var testInput = new UserInput("Astor Place", "6", "Uptown", 1);
+    equal(testInput.time, 1000);
+  });
+
+
 });
