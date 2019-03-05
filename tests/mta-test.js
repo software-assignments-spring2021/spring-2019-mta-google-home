a60d6f7fb01aefa7b7c7033208fe6534e8696d79
DanielY-Yoo <yy2350@nyu.edu>
2019-03-04 21:44:02 -0500

    adding unit test for trainTypes

--- tests/mta-test.js
+++ tests/mta-test.js
@@ -1,16 +1,31 @@
-import { equal } from 'assert';
+import {
+  equal
+} from 'assert';
 import UserInput from '../src/UserInput.js';
 
-describe('UserInput tests', function() {
-  it('UserInput has correct station', function(){
+describe('UserInput tests', function () {
+  it('UserInput has correct station', function () {
     var testInput = new UserInput("Astor Place", "6", "Uptown", 1);
     equal(testInput.station, "Astor Place");
   });
 
-  it('UserInput has Arrival Time ', function(){
+  it('UserInput has Arrival Time ', function () {
     var testInput = new UserInput("Astor Place", "6", "Uptown", 1);
     equal(testInput.time, 1000);
   });
 
+  it('UserInput sets correct line', function () {
+    const testInput = new UserInput("Union Square", "N", "Uptown", 1);
+    equal(testInput.checkTrainType(), true);
+  });
 
+  it('UserInput line checks for cases', function () {
+    const testInput = new UserInput("Union Square", "n", "Uptown", 1);
+    equal(testInput.checkTrainType(), true);
+  });
+
+  it('UserInput checkTrainType checks for invalid lines', function () {
+    const testInput = new UserInput("Union Square", "X", "Uptown", 1);
+    equal(testInput.checkTrainType(), false);
+  });
 });
