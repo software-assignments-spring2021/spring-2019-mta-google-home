02c504dcc908e134dae9756bb972362a80cb3e12
Luis Olivar <lao294@nyu.edu>
2019-04-10 19:32:22 -0400

    Added webhook to app source code, ready to be hooked up with MTA parsing logic

--- src/app.js
+++ src/app.js
@@ -1,16 +1,48 @@
+/*
+  App entry point for parsing MTA requests and handling google actions sdk. 
+*/
+
+// Import the appropriate service and chosen wrappers
+import { dialogflow, Image } from "actions-on-google";
 import express from "express";
-import { urlencoded, json } from "body-parser";
-let app = express();
+import bodyParser from "body-parser";
 
-const PORT = process.env.PORT || 5000;
+// Create an app instance
+const app = dialogflow();
+const expressApp = express().use(bodyParser.json());
 
-app.use(urlencoded({ extended: true }));
-app.use(json());
-
-app.get("/", (req, res) => {
-  res.send(`Hello world, from port ${PORT}`);
+// Register handlers for Dialogflow intents
+app.intent("Default Welcome Intent", conv => {
+  conv.ask("Hi, how is it going?");
+  conv.ask(`Here's a picture of a cat`);
+  conv.ask(
+    new Image({
+      url:
+        "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg",
+      alt: "A cat"
+    })
+  );
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+// Intent in Dialogflow called `Goodbye`
+app.intent("Goodbye", conv => {
+  conv.close("See you later!");
 });
+
+app.intent("Default Fallback Intent", conv => {
+  conv.ask(`I didn't understand. Can you tell me something else?`);
+});
+
+app.fallback(conv => {
+  conv.ask("Welcome! Say a number.");
+});
+
+expressApp.post("/fulfillment", app);
+
+expressApp.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// To start, run ngrok http 3000 after yarn start
+console.log("Listening on port 3000");
+expressApp.listen(3000);
