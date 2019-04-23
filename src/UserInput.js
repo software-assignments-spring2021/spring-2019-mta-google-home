c38106705ec049715bbfb22272f4525f7eebc7cf
DanielY-Yoo <yy2350@nyu.edu>
2019-04-22 21:02:02 -0400

    adding UserInput Singleton Implementation

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,6 +1,6 @@
 class UserInput {
   /**
-   *
+   * Making the UserInput class using SingleTon method because there should be only one instance of UserInput per session.
    * @param {string} station
    * @param {string} trainType
    * @param {string} direction
@@ -8,15 +8,22 @@ class UserInput {
    * @param {time} time
    */
   constructor(station, trainType, direction, numTrains = 1, time = null) {
-    this.station = station;
-    this.trainType = trainType;
-    this.direction = direction;
-    this.numTrains = numTrains;
+    if (!UserInput.instance) {
+      this.station = station;
+      this.trainType = trainType;
+      this.direction = direction;
+      this.numTrains = numTrains;
 
-    if (!time) {
-      time = 1000; // Should set to current time
+      if (!time) {
+        time = new Date().getTime(); // Should set to current time
+      }
+
+      this.time = time;
+
+      UserInput.instance = this;
     }
-    this.time = time;
+
+    return UserInput.instance;
   }
 
   checkTrainType() {
@@ -50,4 +57,7 @@ class UserInput {
   }
 }
 
-export default UserInput;
+const instance = new UserInput();
+Object.freeze(instance);
+
+export default instance;
